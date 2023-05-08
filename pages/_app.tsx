8fae5b825943146b38b58dfae773e687b81b9938
Maria79 <mariajalbisu@gmail.com>
2023-05-08 18:30:58 +0100

    0.1v

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,13 @@ import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout';
 import '@/styles/globals.css';
 import LoginModal from '@/components/modals/LoginModal';
+import RegisterModal from '@/components/modals/RegisterModal';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			{/* <Modal actionLabel='Submit' isOpen title='Test Modal' /> */}
-
+			<RegisterModal />
 			<LoginModal />
 			<Layout>
 				<Component {...pageProps} />
